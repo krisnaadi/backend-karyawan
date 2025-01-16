959d12a9d48fbf1731e2ee620e3e4ef62573cb8e
krisnaadi <man.krisna@gmail.com>
2025-01-16 13:21:46 +0800

    init

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Resources\LoginResource;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+
+        if (!auth()->attempt($credentials)) {
+            return response(['message' => 'Email or password is incorrect'], 401);
+        }
+        return response([
+            'token' => auth()->user()->createToken('authToken')->plainTextToken
+        ]);
+    }
+}
---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreEmployeeRequest;
+use App\Http\Requests\UpdateEmployeeRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return User::paginate(10);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEmployeeRequest $request)
+    {
+        return User::create($request->validated());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return User::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEmployeeRequest $request, string $id)
+    {
+        $user = User::find($id);
+        $user->update($request->validated());
+        return $user;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        return User::destroy($id);
+    }
+}
